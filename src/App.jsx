f92aa1225401946841945a014482d5ab085b8014
Nano <luxorius07@gmail.com>
2024-02-05 03:01:56 -0300

    se realizo practico de HOla Mundo

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
-
+import Navbar from 'react-bootstrap/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Container from 'react-bootstrap/Container';
 import './App.css'
+import Footer from './components/Footer';
 
 function App() {
   
 
   return (
     <>
-      
-    </>
+    <Navbar bg="dark" data-bs-theme="dark">
+        <Container className='container'>
+          <Navbar.Brand href="#home">
+           {' '}
+            Practico React 
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+      <section className='pt-4 mt-4 mainPage'>
+      <h1 className='text-center display-2 my-4'>Hola Mundo!!!</h1>
+      </section>
+      <Footer></Footer>
+      </>
   )
 }
 
